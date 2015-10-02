0cf58656bba60e9142356636877256378463638f
Nicholas Dobbs <nicholas.dobbs@coloradocollege.edu>
2015-10-02 08:49:21 -0600

    adding new javascript

--- server.js
+++ server.js
@@ -3,23 +3,33 @@ var http = require( 'http' );
 
 function getFormValuesFromURL( url )
 {
-    console.log(url)
+    //console.log("GetValuesFromUrl Running!");
+    //console.log(url);
     var kvs = {};
     var parts = url.split( "?" );
     var key_value_pairs = parts[1].split( "&" );
+
+    var wstream = fs.createWriteStream('output.txt', {flags: 'a+'});
+
     for( var i = 0; i < key_value_pairs.length; i++ )
     {
         var key_value = key_value_pairs[i].split( "=" );
         kvs[ key_value[0] ] = key_value[1];
+
+        content_to_file = key_value[1]
+        console.log(content_to_file);
+        wstream.write(content_to_file + '\r\n');
     }
-    // console.log( kvs );
+
+    wstream.end();
+
     return kvs
 }
 
 function server_fun( req, res )
 {
-    console.log( req.url );
-    // ...
+    //console.log("Server_fun Running!")
+    //console.log( req.url );
     var filename = "./" + req.url;
     try {
         var contents = fs.readFileSync( filename ).toString();
@@ -27,12 +37,11 @@ function server_fun( req, res )
         res.end( contents );
     }
     catch( exp ) {
-        // console.log( "huh?", req.url.indexOf( "second_form?" ) );
         if( req.url.indexOf( "first_form?" ) >= 0 )
         {
             var kvs = getFormValuesFromURL( req.url );
             res.writeHead( 200 );
-            res.end( "You submitted the first form. " + kvs[ "number" ] );
+            res.end( "You submitted the first form." );
         }
         else if( req.url.indexOf( "second_form?" ) >= 0 )
         {
@@ -42,7 +51,6 @@ function server_fun( req, res )
         }
         else
         {
-            // console.log( exp );
             res.writeHead( 404 );
             res.end( "Cannot find file: "+filename );
         }
